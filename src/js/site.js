86ff040dfe80e25498ed48ec71df358d7eab6d7a
Katja Hollaar <afkatja@gmail.com>
2016-09-29 12:58:52 +0200

    Polyfill for fetch API

--- src/js/site.js
+++ src/js/site.js
@@ -1,5 +1,6 @@
 require('../sass/assignment.scss');
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import 'whatwg-fetch';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var path = require('path');
 
 module.exports = {
   context: path.join(__dirname, "src"),
+  entry: ['whatwg-fetch'],
   entry: "./js/site.js",
   module: {
     loaders: [
